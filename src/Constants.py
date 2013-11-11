8775f4f1520bd6ce4d5b5d869c635feed0115c84
Roy Tu <thatsnail@gmail.com>
2013-11-10 16:34:49 -0800

    Added getOpposites method

--- src/Constants.py
+++ src/Constants.py
@@ -1,3 +1,15 @@
 """Static namespace only"""
 
 RIGHT, UP, LEFT, DOWN = 0, 1, 2, 3
+
+def getOpposite(direction):
+    """Returns opposite direction
+    >>> getOpposite(LEFT) == RIGHT
+    True
+    >>> getOpposite(UP) == DOWN
+    True
+    >>> getOpposite(DOWN) == LEFT
+    False
+    """
+    opposites = [LEFT, DOWN, RIGHT, UP]
+    return opposites[direction]
