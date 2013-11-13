4df53590e47f467bde1950374f9714973cc6a629
Roy Tu <thatsnail@gmail.com>
2013-11-13 02:28:44 -0800

    Commented maze generation code

--- src/maze.py
+++ src/maze.py
@@ -21,28 +21,42 @@ class Maze(object):
         # TODO
         """Generate maze using Prim's algorithm""" 
         import random
+
         center_x = Maze.width // 2
         center_y = Maze.height // 2
         center_tile = self.getTile(center_x, center_y)
+
+        # Add center tile and center's walls
         cells = [self.getTile(center_x, center_y)]
         walls = [(center_x, center_y, direction) for direction in [RIGHT, UP, LEFT, DOWN]]
+
         while walls != []:
+
+            # Choose a random wall
             wall = walls[random.randint(0, len(walls) - 1)]
             x, y = wall[0], wall[1]
             direction = wall[2]
+
             try:
+                # Get wall's neighboring tile
                 newTile = self.getTileDelta(x, y, direction)
+
+                # If neighbor tile is not already in cells, add it and its walls
                 if newTile not in cells:
+
+                    # Do some magic that I can't be bothered to explain
                     nx, ny = x + getDelta(direction)[0], y + getDelta(direction)[1]
                     self.setTile(x, y, direction, False)
                     cells.append(newTile)
                     for d in [RIGHT, UP, LEFT, DOWN]:
                         newWall = (nx, ny, d)
-                        if newWall not in walls:
+                        newWallAlt = (nx + getDelta(d)[0], ny + getDelta(d)[1], getOpposite(d))
+                        if (newWall not in walls) and (newWallAlt not in walls):
                             walls.append(newWall)
                 else:
                     walls.remove(wall)
             except AssertionError:
+                # If the wall's neighbor is outside of the grid, remove it
                 walls.remove(wall)
                 
         pass
